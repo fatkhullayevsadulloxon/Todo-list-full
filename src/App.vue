193031806dba332e6be8af2487a51c4393c5eff2
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2022-12-29 22:56:50 +0000

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,124 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  });
+
+  input_content.value = "";
+  input_category.value = null;
+};
+
+const removeTodo = (todo) => {
+    todos.value = todos.value.filter(t => t !== todo)
+}
+
+watch(
+  todos,
+  newVal => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
+
+watch(name, newVal => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <main class="app">
+      <section class="greeting">
+          <h2 class="title">
+              Nima gap, <input type="text" placeholder="ismingizni kiriting" v-model="name"/>
+          </h2>
+      </section>
+
+      <section class="create-todo">
+        <h3>TOPSHIRIQ YARATISH</h3>
+      
+        <form @submit.prevent="addTodo">
+          <h4>Vazifalar ro'yxatiga nimalar kiritmoqchisiz?</h4>
+          <input 
+            type="text" 
+            placeholder="masalan: texnomartdan noutbuk sotib olish" 
+            v-model="input_content" />
+
+          <h4>Categoriyani tanlang</h4>
+          <div class="options">
+
+            <label>
+              <input 
+               type="radio"
+               name="category" 
+               value="business" 
+               v-model="input_category"/>
+              <span class="bubble business"></span>
+              <div>Biznes</div>
+            </label>
+            
+            <label>
+              <input 
+               type="radio"
+               name="category" 
+               value="personal" 
+               v-model="input_category"/>
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+            </label>
+
+            <input type="submit" value="Add Todo">
+          </div>
+        </form>
+      </section>
+
+      <section class="todo-list">
+        <h3>TODO LIST</h3>
+        <div class="list">
+
+              <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+
+                  <label>
+                      <input type="checkbox" v-model="todo.done"/>
+                      <span :class="`bubble ${todo.category}`"></span>
+                  </label>
+
+                  <div class="todo-content">
+                      <input type="text" v-model="todo.content">
+                  </div>
+
+                  <div class="actions">
+                      <button class="delete" @click="removeTodo(todo)">Delete</button>
+                  </div>
+
+              </div>
+
+        </div>
+      </section>
+    </main>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
